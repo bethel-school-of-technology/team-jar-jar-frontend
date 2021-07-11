1e8946c6b272825cded3c69c0a65766a716b4a1e
joppajoe <joeacn08@gmail.com>
2021-07-10 21:53:25 -0400

    added screenshot

--- src/App.js
+++ src/App.js
@@ -3,36 +3,32 @@ import {
   BrowserRouter as Router,
   Switch,
   Route,
-  Link
+  NavLink
 } from "react-router-dom";
 import Login from './components/Login'
 import Saber from './components/Saber.js';
-// import './components/ScreenCapture';
-// import ScreenCaptureComponent from './components/ScreenCapture'; 
 import PrivateRoute from './components/PrivateRoute';
 import '../src/App.css';
 import { Navbar } from 'react-bootstrap';
-// import 'bootstrap/dist/css/bootstrap.min.css'  ***might use???
 
 // Sean had import statements for screenshotcapture on this page ; in process of switching screencapture libraries
 // the rest is joeys
 // place Form and screencapture components in saber.js
+
 function App() {
+
   return (
     <div className="App">
       <Router>
       <div>
 
         {/* figure out navbar react bootstrap */}
-
-    <Navbar bg="dark" variant="dark" >
-        <Link to="/Login">Login</Link>
-        <Link to="/Saber">Saber</Link>
+    <Navbar className="navbar">
+      <NavLink to="/Login" className="navlink" id="loginlink">Login</NavLink><br/>
+        <NavLink to="/Saber" className="navlink">Build a Lightsaber</NavLink>  
     </Navbar>
 
-
       </div>
-      {/* test routes to see if they are working proerly */}
       <Switch>
       <Route path="/Login">
             <Login />
--- src/components/Saber.js
+++ src/components/Saber.js
@@ -1,43 +1,38 @@
 import React from 'react';
-import './Saber.js';
 import './Saber.css';
+import html2canvas from 'html2canvas';
 
-// all seans
 
 function Saber() {
+
+    html2canvas(document.querySelector("#capture")).then(canvas => {
+        document.body.appendChild(canvas)
+    });
    
     return(
-<div>
-<h4>
-<label for="green">Green</label>
-    <input type="radio" id="green" name="color" checked />
-    <label for="blue">Blue</label>
-    <input type="radio" id="blue" name="color" />
-    <label for="yellow">Yellow</label>
-    <input type="radio" id="yellow" name="color" />
-    <label for="purple">Purple</label>
-    <input type="radio" id="purple" name="color" />
-    <label for="red">Red</label>
-    <input type="radio" id="red" name="color" />
-    <label for="darksaber">Darksaber</label>
-    <input type="radio" id="darksaber" name="color" />
-   
-    <div class="lightsaber" id="lightSaber">
-        <input type="checkbox" id="on-off" />
-        <div class="blade"></div>
-        <label class="hilt" for="on-off"> </label>
-    </div>
-  
-  {/* <iframe width="0" height="0" controls src="https://www.youtuberepeater.com/watch?v=XHxOg6zNg9Q#gsc.tab=0" frameborder="0"
+<div id="capture">
+    <h4>
+        <label for="green">Green</label>
+            <input type="radio" id="green" name="color" checked />
+            <label for="blue">Blue</label>
+            <input type="radio" id="blue" name="color" />
+            <label for="yellow">Yellow</label>
+            <input type="radio" id="yellow" name="color" />
+            <label for="purple">Purple</label>
+            <input type="radio" id="purple" name="color" />
+            <label for="red">Red</label>
+            <input type="radio" id="red" name="color" />
+            <label for="darksaber">Darksaber</label>
+            <input type="radio" id="darksaber" name="color" />
+        
+            <div class="lightsaber" id="lightSaber">
+                <input type="checkbox" id="on-off" />
+                <div class="blade"></div>
+                <label class="hilt" for="on-off"> </label>
+            </div>
 
-  allowfullscreen></iframe> */}
-
-
-</h4>
+    </h4>
 </div>
-
-
-
 
     );
 
